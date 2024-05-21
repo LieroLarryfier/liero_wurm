f19c6e5f0926cdd4db100ad2ef937e863c2cb061
LieroLarryfier <lierolarryfier@jamesc.at>
2024-05-21 20:26:30 +0200

    reset with esc;

--- src/game/input.rs
+++ src/game/input.rs
@@ -1,7 +1,9 @@
-use crate::snake::{Body, Dead, Direction, Head, Player1Marker};
+use crate::snake::{Body, BodyMarker, Dead, Direction, Head, Player1Marker, SnakeBundle};
 use bevy::ecs::{query::With, system::{Query, Res}};
 use bevy::prelude::*;
 use crossterm::event::Event;
+
+use super::Game;
 
 pub fn handle_input(input: Res<ButtonInput<KeyCode>>, mut query: Query<&mut Direction, With<Player1Marker>>) {
     for mut direction in &mut query {
@@ -28,12 +30,51 @@ pub fn handle_input(input: Res<ButtonInput<KeyCode>>, mut query: Query<&mut Dire
     }
 }
 
-pub fn handle_reset(input: Res<ButtonInput<KeyCode>>, mut dead_query: Query<(Entity, &mut Dead)>, mut commands: Commands) { 
+pub fn handle_reset(input: Res<ButtonInput<KeyCode>>, mut dead_query: Query<(Entity, &mut Dead)>, mut game: ResMut<Game>, mut commands: Commands, asset_server: Res<AssetServer>, mut texture_atlas_layouts: ResMut<Assets<TextureAtlasLayout>>,) { 
 
     let (entity, mut dead) = dead_query.single_mut();
     if input.just_pressed( KeyCode::Escape) {
         println!("dead");
         commands.entity(entity).despawn();   
+
+        game.score = 0;
+
+        let layout = TextureAtlasLayout::from_grid(Vec2::new(10.0, 10.0), 5, 1, None, None);
+  
+        commands.spawn((
+            Player1Marker,
+            Dead(false),
+            SnakeBundle {
+                ..Default::default()
+            },
+            SpriteSheetBundle {
+                texture: asset_server.load("sprite.png"),
+                atlas: TextureAtlas {
+                    layout: texture_atlas_layouts.add(layout.clone()),
+                    index: 0
+                },
+                transform: Transform::from_translation(Vec3::new(-10.0, -10.0, 1.0)),
+                ..default()
+            }
+        ));
+
+        let default_body = SnakeBundle::default().body;
+
+        for pos in &default_body.0 {
+            println!("add_body");
+            commands.spawn((
+                BodyMarker,
+                SpriteSheetBundle {
+                    texture: asset_server.load("sprite.png"),
+                    atlas: TextureAtlas {
+                        layout: texture_atlas_layouts.add(layout.clone()),
+                        index: 4
+                    },      
+                transform: Transform::from_translation(Vec3::new(pos.x.into(), pos.y.into(), 0.0)),
+                ..default()
+                }
+            ));
+        }   
     }
 }
 
